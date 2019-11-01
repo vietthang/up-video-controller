477639b24098297a03b8f41595007a64a3a6138b
Thang Nguyen <thangnv77@gmail.com>
2019-11-01 10:29:02 +0700

    Initialize renderer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from 'react'
+import { Canvas } from 'react-three-fiber'
+import { StringParam, useQueryParam } from 'use-query-params'
+import './App.css'
+import { Scene, SceneProps } from './Scene'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface UpdateSceneMessage {
+  type: 'updateScene'
+  payload: SceneProps
 }
 
-export default App;
+const App: React.FC = () => {
+  const [sceneProps, setSceneProps] = useState<SceneProps>({
+    videoUrl: './FILAMENT_WORK_SLOW_01.mp4',
+    viewPort: { left: 0, top: 0, width: 1280, height: 720 },
+    samplers: [
+      {
+        input: { left: 0, top: 0, width: 5376 / 2, height: 192 },
+        output: { left: 0, top: 0, width: 640, height: 192 },
+      },
+      {
+        input: { left: 5376 / 2, top: 0, width: 5376 / 2, height: 192 },
+        output: { left: 640, top: 0, width: 640, height: 192 },
+      },
+    ],
+  })
+
+  const [q] = useQueryParam('props', StringParam)
+
+  useMemo(() => {
+    if (!q) {
+      return
+    }
+    try {
+      const props = JSON.parse(q)
+      setSceneProps(props)
+    } catch {
+      // no need to do anything
+    }
+  }, [q])
+
+  useMemo(() => {
+    import('electron')
+      .then(electron => {
+        const ipc = electron.ipcRenderer
+        ipc.on('message', (evt, message) => {
+          if (message.type !== 'updateScene') {
+            return
+          }
+          setSceneProps(message.payload)
+        })
+      })
+      .catch(error =>
+        console.log(
+          'can not import electron, maybe it is not run by electron',
+          error,
+        ),
+      )
+
+    window.addEventListener('message', evt => {
+      if (evt.data !== 'updateScene') {
+        return
+      }
+      setSceneProps(evt.data.payload)
+    })
+  }, [])
+
+  return (
+    <Canvas
+      invalidateFrameloop={false}
+      style={{
+        left: sceneProps.viewPort.left,
+        top: sceneProps.viewPort.top,
+        width: sceneProps.viewPort.width,
+        height: sceneProps.viewPort.height,
+      }}
+      orthographic={true}
+    >
+      <Scene {...sceneProps}></Scene>
+    </Canvas>
+  )
+}
+
+export default App
