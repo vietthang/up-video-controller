14e06d46e99868d905a78eb9619f29b8ab5fcc5c
Thang Nguyen <thangnv77@gmail.com>
2019-11-05 09:49:26 +0700

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
-import React, { useMemo, useState } from 'react'
+import { IpcRendererEvent } from 'electron'
+import React, { DependencyList, useEffect, useState } from 'react'
 import { Canvas } from 'react-three-fiber'
 import { StringParam, useQueryParam } from 'use-query-params'
 import './App.css'
 import { Scene, SceneProps } from './Scene'
 
-export interface UpdateSceneMessage {
-  type: 'updateScene'
-  payload: SceneProps
+type Resolvable<T> = T | Promise<T>
+
+type EffectAyncCallback = () => Resolvable<
+  void | (() => Resolvable<void | undefined>)
+>
+
+export type AsyncState =
+  | { state: 'pending' }
+  | { state: 'finished' }
+  | { state: 'error'; error: any }
+
+export function useEffectAsync(
+  callback: EffectAyncCallback,
+  deps?: DependencyList,
+): AsyncState {
+  const [effectState, setEffectState] = useState<AsyncState>({
+    state: 'pending',
+  })
+
+  useEffect(() => {
+    let active = true
+    let unmountCallback: (() => Resolvable<void | undefined>) | undefined
+
+    Promise.resolve(callback())
+      .then(ret => {
+        setEffectState({ state: 'finished' })
+
+        if (!ret) {
+          return
+        }
+        if (active) {
+          unmountCallback = ret
+          return
+        }
+        // resolved after unmount, just call the release
+        return ret()
+      })
+      .catch(error => setEffectState({ state: 'error', error }))
+
+    return () => {
+      active = false
+      if (unmountCallback) {
+        Promise.resolve(unmountCallback()).catch(error =>
+          setEffectState({ state: 'error', error }),
+        )
+      }
+    }
+  }, deps)
+
+  return effectState
 }
 
 const App: React.FC = () => {
@@ -16,18 +64,18 @@ const App: React.FC = () => {
     samplers: [
       {
         input: { left: 0, top: 0, width: 5376 / 2, height: 192 },
-        output: { left: 0, top: 0, width: 640, height: 192 },
+        output: { left: 0, top: 0, width: 360, height: 192 },
       },
       {
         input: { left: 5376 / 2, top: 0, width: 5376 / 2, height: 192 },
-        output: { left: 640, top: 0, width: 640, height: 192 },
+        output: { left: 640, top: 0, width: 360, height: 192 },
       },
     ],
   })
 
   const [q] = useQueryParam('props', StringParam)
 
-  useMemo(() => {
+  useEffect(() => {
     if (!q) {
       return
     }
@@ -39,31 +87,33 @@ const App: React.FC = () => {
     }
   }, [q])
 
-  useMemo(() => {
-    import('electron')
-      .then(electron => {
-        const ipc = electron.ipcRenderer
-        ipc.on('message', (evt, message) => {
-          if (message.type !== 'updateScene') {
-            return
-          }
-          setSceneProps(message.payload)
-        })
-      })
-      .catch(error =>
-        console.log(
-          'can not import electron, maybe it is not run by electron',
-          error,
-        ),
-      )
+  useEffectAsync(async () => {
+    const { ipcRenderer } = await import('electron')
 
-    window.addEventListener('message', evt => {
+    const ipcEventHandler = (evt: IpcRendererEvent, message: any) => {
+      setSceneProps(message.payload)
+    }
+
+    ipcRenderer.on('updateScene', ipcEventHandler)
+    return () => {
+      ipcRenderer.off('updateScene', ipcEventHandler)
+    }
+  })
+
+  useEffect(() => {
+    const windowMessageHnadler = (evt: MessageEvent) => {
       if (evt.data !== 'updateScene') {
         return
       }
       setSceneProps(evt.data.payload)
-    })
-  }, [])
+    }
+
+    window.addEventListener('message', windowMessageHnadler)
+
+    return () => {
+      window.removeEventListener('message', windowMessageHnadler)
+    }
+  })
 
   return (
     <Canvas
